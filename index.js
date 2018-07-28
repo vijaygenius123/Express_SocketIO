69550bfa7d63e31bf0bfee72849fdda55fdb94a7
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2018-07-28 11:04:35 +0530

    Can Send Message To Backend. Logging Message Received From chat message

--- index.js
+++ index.js
@@ -7,9 +7,18 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+    console.log('user connected');
+});
+
+io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
   });
 });
 
